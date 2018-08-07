c268e706087a7cd6b259d7f77792e797a80f78d3
lauranadolski <lauranadolski@users.noreply.github.com>
2018-08-06 20:44:55 -0400

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
 export default class ColorBox extends Component {
-  
+
   render() {
+    const newOpacity = this.props.opacity - 0.1;
+    console.log(`This is the old opacity: ${this.props.opacity}`)
+    console.log(`This is the new opacity: ${newOpacity}`)
+
     return (
-      <div className="color-box" style={{opacity: 2}}>
-        {/* your conditional code here! */}
-      </div>
+     <div className="color-box" style={{ opacity: this.props.opacity }}>
+      {this.props.opacity >= 0.2 ? <ColorBox opacity={ this.props.opacity - 0.1 }/> : null}
+     </div>
     )
+    }
   }
-  
-}
---
+++ src/Comment.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// are we missing an import?
+
+export default class Comment extends Component {
+  render() {
+    return (
+
+      <div className="comment">
+        {this.props.commentText}
+      </div>
+
+    )
+  }
+}
